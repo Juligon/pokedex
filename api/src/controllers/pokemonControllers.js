cc8c97ce900b3a1380ed7fc2340c26ab12b1a30a
Juligon <juligon.dev@gmail.com>
2023-12-13 17:45:11 -0300

    fixing bugs

--- api/src/controllers/pokemonControllers.js
+++ api/src/controllers/pokemonControllers.js
@@ -2,68 +2,65 @@ require("dotenv").config();
 const axios = require("axios");
 const { API_URL } = process.env;
 
-// Función para obtener la lista básica de Pokémon con nombre e imagen y filtrar por nombre o id
+// Función para obtener lista básica de Pokémon con nombre e imagen, filtrar por nombre o id y paginado
 const getPokemons = async (req, res, next) => {
-  try {
-    console.log("Request to /api/pokemons", req.query);
+	try {
+		let { name, id, page = 1, limit = 20 } = req.query;
+		let apiUrl = API_URL;
 
-    let { name, id, page = 1, limit = 20 } = req.query; // Cambié el límite a 20 ya que parece que quieres cargar 20 pokémons por página
-    let apiUrl = API_URL;
+		if (name || id) {
+			apiUrl = `https://pokeapi.co/api/v2/pokemon/${name || id}`;
+		}
 
-    if (name || id) {
-      apiUrl = `https://pokeapi.co/api/v2/pokemon/${name || id}`;
-    }
+		const response = await axios.get(apiUrl);
 
-    const response = await axios.get(apiUrl);
+		if (response.data.results) {
+			const pokemonResults = response.data.results;
 
-    if (response.data.results) {
-      const pokemonResults = response.data.results;
+			// Obtiene la siguiente página
+			let nextPageUrl = response.data.next;
+			let currentPage = 1;
 
-      // Obtiene la siguiente página
-      let nextPageUrl = response.data.next;
-      let currentPage = 1;
+			while (nextPageUrl && currentPage < page) {
+				const nextPageResponse = await axios.get(nextPageUrl);
+				pokemonResults.push(...nextPageResponse.data.results);
+				nextPageUrl = nextPageResponse.data.next;
+				currentPage++;
+			}
 
-      while (nextPageUrl && currentPage < page) {
-        const nextPageResponse = await axios.get(nextPageUrl);
-        pokemonResults.push(...nextPageResponse.data.results);
-        nextPageUrl = nextPageResponse.data.next;
-        currentPage++;
-      }
+			// Paginación
+			const start = (page - 1) * limit;
+			const end = start + limit;
+			const paginatedResults = pokemonResults.slice(start, end);
 
-      // Paginación
-      const start = (page - 1) * limit;
-      const end = start + limit;
-      const paginatedResults = pokemonResults.slice(start, end);
+			let pokemons = await Promise.all(
+				paginatedResults.map(async (pokemon) => {
+					const details = await axios.get(pokemon.url);
+					return {
+						id: details.data.id,
+						name: details.data.name,
+						image: details.data.sprites.front_default,
+					};
+				})
+			);
 
-      let pokemons = await Promise.all(
-        paginatedResults.map(async (pokemon) => {
-          const details = await axios.get(pokemon.url);
-          return {
-            id: details.data.id,
-            name: details.data.name,
-            image: details.data.sprites.front_default,
-          };
-        })
-      );
+			if (pokemons.length === 0) {
+				return res.status(404).json({ message: "No Pokemons found" });
+			}
 
-      if (pokemons.length === 0) {
-        return res.status(404).json({ message: "No Pokemons found" });
-      }
-
-      res.json(pokemons);
-    } else {
-      const pokemon = {
-        id: response.data.id,
-        name: response.data.name,
-        image: response.data.sprites.front_default,
-      };
-      res.json([pokemon]);
-    }
-  } catch (error) {
-    next(error);
-  }
+			res.json(pokemons);
+		} else {
+			const pokemon = {
+				id: response.data.id,
+				name: response.data.name,
+				image: response.data.sprites.front_default,
+			};
+			res.json([pokemon]);
+		}
+	} catch (error) {
+		next(error);
+	}
 };
-
 
 // Función para obtener los detalles completos de un Pokémon específico
 const getPokemonDetails = async (req, res, next) => {
