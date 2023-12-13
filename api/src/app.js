b7e3062d3b5efed986b96c4b9399e75562a36120
Juligon <juligon.dev@gmail.com>
2023-12-13 14:20:55 -0300

    fixing bugs

--- api/src/app.js
+++ api/src/app.js
@@ -13,20 +13,20 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
-	res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
 // Error catching endware.
 server.use((err, req, res, next) => {
-	// eslint-disable-line no-unused-vars
-	const status = err.status || 500;
-	const message = err.message || err;
-	console.error(err);
-	res.status(status).send(message);
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
 });
 
 module.exports = server;
