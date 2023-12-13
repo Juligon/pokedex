a35371cc1d66b808c40b4c08e16df21775dbea19
Juligon <juligon.dev@gmail.com>
2023-12-13 14:56:03 -0300

    fixing bugs

--- api/src/app.js
+++ api/src/app.js
@@ -5,10 +5,11 @@ const morgan = require("morgan");
 const server = express();
 server.name = "API";
 
+server.use(cors());
+
 const routes = require("./routes/index.js");
 server.use("/api", routes);
 
-server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(morgan("dev"));
