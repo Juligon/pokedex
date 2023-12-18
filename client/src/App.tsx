229ead9f6c376d056e19d95148c3c3b37716fb8e
Juligon <juligon.dev@gmail.com>
2023-12-18 19:43:09 -0300

    logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,7 +34,7 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-import Logo from "./assets/logo2.svg";
+import Logo from "./assets/logo2.png";
 
 setupIonicReact();
 
@@ -54,7 +54,7 @@ const App: React.FC = () => (
 				<IonTabBar slot="bottom">
 					<IonTabButton tab="allPokemons" href="/">
 						{/* <IonIcon aria-hidden="true" icon={square} size="small" /> */}
-						<IonImg aria-hidden="true" src={Logo} style={{height: "35px", color: "#fff"}}/>
+						<IonImg aria-hidden="true" src={Logo} style={{height: "25px", paddingBottom: "5px"}}/>
 						<IonLabel style={{color: "#fff"}}>Pokedex</IonLabel>
 					</IonTabButton>
 				</IonTabBar>
