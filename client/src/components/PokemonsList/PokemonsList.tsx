90d82b3d45026a6e52d8afcd0f7243a2698b9440
Juligon <juligon.dev@gmail.com>
2023-12-18 13:05:59 -0300

    adding types

--- client/src/components/PokemonsList/PokemonsList.tsx
+++ client/src/components/PokemonsList/PokemonsList.tsx
@@ -96,22 +96,22 @@ const PokemonList: React.FC<PokemonListProps> = ({ search }) => {
 			grass: "#78C850",
 			fire: "#F08030",
 			water: "#6890F0",
-      normal: "#AAA67F",
-      fighting: "#C12239",
-      flying: "#A891EC",
-      ground: "#DEC16B",
-      poison: "#A43E9E",
-      rock: "#B69E31",
-      bug: "#A7B723",
-      ghost: "#70559B",
-      steel: "#B7B9D0",
-      electric: "#F9CF30",
-      psychic: "#FB5584",
-      ice: "#9AD6DF",
-      dragon: "#7037FF",
-      dark: "#75574C",
-      fairy: "#E69EAC",
-      shadow: "#453F3A" 
+			normal: "#AAA67F",
+			fighting: "#C12239",
+			flying: "#A891EC",
+			ground: "#DEC16B",
+			poison: "#A43E9E",
+			rock: "#B69E31",
+			bug: "#A7B723",
+			ghost: "#70559B",
+			steel: "#B7B9D0",
+			electric: "#F9CF30",
+			psychic: "#FB5584",
+			ice: "#9AD6DF",
+			dragon: "#7037FF",
+			dark: "#75574C",
+			fairy: "#E69EAC",
+			shadow: "#453F3A",
 		};
 		//@ts-ignore
 		return typeColorMap[type] || "#A8A878";
@@ -140,18 +140,19 @@ const PokemonList: React.FC<PokemonListProps> = ({ search }) => {
 										<IonCardTitle>
 											{capitalizeFirstLetter(pokemon.name)}
 										</IonCardTitle>
-										<IonCardContent>
-											{/* @ts-ignore */}
-											{pokemon.types.map((type, index) => (
-												<IonChip
-													key={index}
-													style={{ backgroundColor: mapTypeToColor(type) }}
-												>
-													<IonLabel style={{ color: "white" }}>{type}</IonLabel>
-												</IonChip>
-											))}
-										</IonCardContent>
 									</IonCardHeader>
+									<IonCardContent>
+										{/* @ts-ignore */}
+										{pokemon.types.map((type, index) => (
+											<IonChip
+												slot="start"
+												key={index}
+												style={{ backgroundColor: mapTypeToColor(type) }}
+											>
+												<IonLabel style={{ color: "white" }}>{type}</IonLabel>
+											</IonChip>
+										))}
+									</IonCardContent>
 								</IonCard>
 							</IonRouterLink>
 						</IonCol>
